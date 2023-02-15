b380e8e73463a3bda90f02bf96757f4de024c471
BarakaMukiza <barakakinyua2@gmail.com>
2023-02-15 10:52:54 +0300

    odd_even

--- week-2/for_loop.py
+++ week-2/for_loop.py
@@ -1,2 +1,20 @@
-for i in range (0,7):
+for i in range (0,10):
     print(i)
+
+for i in range (0,100):
+    print(i)
+
+for i in range (20,100):
+    print(i)
+
+sum = 0
+for i in range (0,10):
+    sum = sum + i
+
+print(sum)
+
+#print product of the first 10 numbers
+product = 1
+for x in range (1,10):
+    product = product * x
+    print(product)
