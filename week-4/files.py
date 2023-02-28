73383cd4156f62456c6efad0dd2c9908df0b25a4
BarakaMukiza <barakakinyua2@gmail.com>
2023-02-28 08:11:07 +0300

    week-4

--- week-4/files.py
+++ week-4/files.py
@@ -1,6 +1,5 @@
+f = open(r'C:\\Users\\THE KINOTI FAMILY\Documents\\Inspire-Youth-In-STEM\\week-4\\text.txt')
+print(f.readline())
 
 
 
-file_name = ("C:\\Users\\THE KINOTI FAMILY\Documents\\Inspire-Youth-In-STEM\\week-4\\text.txt","r+w+a")
-f = open(file_name,"r + w",encoding = None)
-print(f.readlines())
